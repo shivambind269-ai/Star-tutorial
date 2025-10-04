d6102b6b69df6b2f22e530b337d0df662ac7b715
Shivam <shivambind269@gmail.com>
2025-10-04 21:17:30 +0530

    Added practice

---
+++ practice.py
@@ -0,0 +1,66 @@
+# 1.
+'''a = int(input("ask to user :"))
+b = int(input("ask to user :"))
+sum = a+b
+print(sum)
+
+#2
+
+l = int(input("inter length of side :"))
+area = l*l
+print(area)
+
+#3
+
+a = float(input("take from user :"))
+b = float(input("take from user :"))
+c = float(input("take from user :"))
+sum = a+b+c
+avg = sum/3
+print(avg)
+
+#3(1)
+
+a = float(input("enter your value:"))
+b = float(input("enter your value:"))
+sum = a+b
+avg = sum/2
+print(avg)
+
+#4
+
+a = int(input("enter your number :"))
+b = int(input("enter your number :"))
+if a>=b:
+    print(True)
+else:
+    print(False)
+
+#5,6
+
+str = "Shivam $ Kum$ar Bi$nd"
+#print(len(str)) 
+#print(str[:6]) 
+#print(str.capitalize())
+#print(str.replace("Kumar", "a"))
+#print(str.find("n"))
+print(str.count("$"))
+
+#7
+
+nums =  int(input("enter a number :"))
+rem = nums%2
+if (rem == 0):
+    print("Even")
+else:
+    print("Odd")'''
+
+#8
+
+
+
+
+
+
+
+
